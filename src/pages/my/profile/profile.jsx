60e49d2effa47bcb37f7ff3d70c56c82ce211895
Fajar Alfath <mfalfath25@gmail.com>
2021-11-24 16:34:33 +0700

    Add login & profile edit page

---
+++ src/pages/my/profile/profile.jsx
@@ -0,0 +1,11 @@
+import ProfileUser from "../../../components/pages/my/profile/profile-user";
+
+const Profile = () => {
+  return (
+    <div>
+      <ProfileUser />
+    </div>
+  );
+};
+
+export default Profile;
---
+++ src/pages/register/register.jsx
@@ -0,0 +1,7 @@
+import { Box } from "@chakra-ui/layout";
+
+const Register = () => {
+  return <Box>Ini register page</Box>;
+};
+
+export default Register;
